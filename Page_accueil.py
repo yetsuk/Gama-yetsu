d793bfc6ded3c35544ce18b66ac7e4b52f6ef46f
yetsuk <yetsu_kun@outlook.com>
2025-01-06 11:27:01 +0100

    Ajout header et subheader

--- Page_accueil.py
+++ Page_accueil.py
@@ -5,3 +5,24 @@ st.set_page_config(
         layout="wide"
     )
 
+
+
+# --- Titre de le Page ---
+
+# Centrer le header
+st.markdown(
+    """
+    <h1 style='text-align: center;'>Projet - GAMA</h1>
+    """,
+    unsafe_allow_html=True
+)
+
+# --- Sous-titre de la Page ---
+
+st.markdown(
+    """
+    <h3 style='text-align: center; color: grey;'>Extraction et Analyse de publication Reddit</h3>
+    """,
+    unsafe_allow_html=True
+)
+
