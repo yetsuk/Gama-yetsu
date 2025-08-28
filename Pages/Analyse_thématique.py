4ef616c3cf719efc18914ccbee44907945372c39
yetsuk <yetsu_kun@outlook.com>
2025-08-28 17:06:21 +0200

    Suppression analyse Query et Commentaire

--- Pages/Analyse_thématique.py
+++ Pages/Analyse_thématique.py
@@ -43,15 +43,9 @@ load_dotenv(dotenv_path=dotenv_path)
 
 st.title('Analyse textuelle Reddit')
 st.write(''' 
-         Cette page est un script réalisé avec Python qui a pour principe d'extraire puis analyser un corpus de texte issue de l'application
-         Reddit. En premier lieu, le script propose de sélectionner le type d'élement que vous voulez extraire entre trois options: les posts de subreddit,
-         les commentaires d'un même post, ou un ensemble de post trouvé à la suite d'une query. Dans le cas des publications des subreddits, le code propose
-         une seconde option qui est la sélection entre les titres des publications et le contenu de la publication qui donne d'avantage d'information sur les 
-         sujets abordés en son sein. 
-         
-         L'objectif principale est de réaliser une analyse thématique des publications Reddit afin d'en retenir les sujets qui peuvent abordés dans les conversations
-         des membres du réseau. Cette analyse se fait uniquement avec Python et différents packages permettant l'analyse de donnée textuelle comme spacy et sklearn.
-         Les résultats obtenus sont ensuite visualisé à l'aide plusieurs graphes qui seront affiché en fin de processus. ''')
+         Désigner le subreddit de votre choix afin d'obtenir une liste de thèmes abordés par les membres
+         de la communauté. Il vous suffit juste d'insérer le noms de la communauté, le programme s'occupera
+         de l'extraction et de l'analyse des publications. ''')
 
 
 # --- Ajout Instance de connexion Reddit --- 
@@ -71,13 +65,6 @@ except Exception as e:
 # Définition des Fonctions pour analyse thématique
 ##################################################
 
-# --- Choix des critères d'extraction ---
-def choix_extraction():
-    choix_extract = st.radio("Veuillez choisir le type d'extraction souhaitez",
-                         ['Publication Subreddit', 'Commentaire', 'Query'])
-
-    st.write(f'Vous avez choisis: {choix_extract}')
-
 
 # Choix du corpus à analyser
 def choix_corpus():
@@ -85,17 +72,7 @@ def choix_corpus():
     choix_corpus = st.radio('Veuillez choisir les corpus que vous souhaitez analyser', list_corpus )
     st.write(f'Vous avez choisis: {choix_corpus}')
 
-
-
-# Choix source de recherche
-def choix_source():
-    list_source = ['Lien', 'N° Identification']
-    choix_source = st.radio('Veuillez Choisir la source qui va vous servir de requête', list_source)
-    st.write(f'Vous avez choisis: {choix_source}')
-    return choix_source
-
-
-
+""
 # --- Extraction des publications Reddit ---
 def extract_post(communaute, choix_corpus):
 
@@ -142,63 +119,6 @@ def display_post_info(post):
     st.write(post.selftext)
 
 
-
-# Nettoyage des liens
-def from_link_get_id(link):
-
-    # Nettoyage du lien pour obtenir l'id du post
-    if link:
-        url = link.split('/')
-
-        # Vérifiez que l'URL a au moins 7 segments
-        if len(url) > 6:
-            post_id = url[6]
-            st.write(f"ID du post extrait : {post_id}")
-        else:
-            st.error("Lien invalide. Veuillez entrer un lien de post Reddit complet.")
-            post_id = None
-
-    return post_id
-
-
-# Extraction des commentaires
-def extract_comment(post):
-    comments_dict = {}
-
-    post.comments.replace_more(limit=5)
-    all_comments = post.comments.list()
-
-    for comment in all_comments:
-        comments_dict[comment.id] = {
-            'comment_author': comment.author.name if comment.author else '[deleted]',
-            'Texte': comment.body}
-
-    # Création d'une Dataframe pour y ajouter les commentaires et les différentes caractéristiques associés
-    comment_df = pd.DataFrame(comments_dict)
-    comment_df = comment_df.transpose()
-
-    return comment_df
-
-
-
-# Extraction des posts depuis une query
-def from_query_get_post(query):
-    subreddit = reddit.subreddit("all")  # Recherche dans tout Reddit, ou spécifie un subreddit
-
-    # Extraire les publications contenant le terme recherché
-    post_dict = {'Auteur': [], 'Texte': []}
-    for submission in subreddit.search(query, limit= None):  # Ajuste le "limit" pour plus ou moins de résultats
-        post_dict['Auteur'].append(submission.author)
-
-        post_dict['Titre'].append(submission.title)
-
-
-
-    comment_df = pd.DataFrame(post_dict)
-
-    return comment_df
-
-
 st.divider()
 
 ##################################
@@ -214,7 +134,6 @@ choix_extract = st.radio("Veuillez choisir le type d'extraction souhaitez !",
 
 
 # Initialisation variable publication
-df_post = pd.DataFrame()
 
 st.write(f'Vous avez choisis: {choix_extract}')
 st.divider()
@@ -252,80 +171,6 @@ if choix_extract == 'Publication Subreddit':
         st.write('Veuillez cliquer sur le bouton pour valider !')
 
 
-elif choix_extract == 'Commentaire':
-    ## Sélection de la source Souhaiter
-    list_source = ['Lien', 'N° Identification']
-    choix_source = st.radio('Veuillez Choisir la source qui va vous servir de requête', list_source)
-    st.write(f'Vous avez choisis: {choix_source}')
-
-    if choix_source == 'Lien':
-
-        link = st.text_input('Veuillez ajoutez le lien du post à analyser !')
-
-        if st.button('Validez'):
-
-            if link:
-                st.success('Analyse en cours')
-                try:
-                    # Extraction ID
-                    id_post = from_link_get_id(link)
-
-                    # Extraire la publication Reddit
-                    post = reddit.submission(id = id_post)
-
-                    # Extraction des commentaires
-                    df_post = extract_comment(post)
-
-                except Exception as e:
-                    st.error("Erreur lors de la récupération des informations du subreddit. Veuillez vérifier le nom ou réessayer plus tard.")
-
-        else:
-            st.write('Veuillez cliquer sur le bouton pour valider !')    
-
-
-    else:
-        id_post = st.text_input("Veuillez ajoutez l'ID de la publication !")
-        
-
-        if st.button('Validez'):
-
-            if id_post:
-                st.succes('Analyse en cours')
-                try:
-
-                    post = reddit.submission(id = id_post)
-            
-                    df_post = extract_comment(post)
-        
-                except Exception as e:
-                    st.error("Erreur lors de la récupération des informations du subreddit. Veuillez vérifier le nom ou réessayer plus tard.")
-
-        else:
-            st.write('Veuillez cliquer sur le bouton pour valider !') 
-
-else: #Query
-    
-
-     # Extraction des publications de subreddit
-    query = st.text_input('Veullez écrire le terme à rechercher !')
-    st.divider()
-
-    if st.button('Validez'):
-
-        if query:
-            # Ecrit un message pour avertir du début de la recherche
-            st.succes('Analyse en cours')
-            st.subheader(f'Recherche de publication correspondant à "{query}"')
-            # Retourne des informations sur la communauté
-            try:
-                df_post = from_query_get_post(query)
-
-
-            except Exception as e:
-                st.error("Erreur lors de la récupération des informations du subreddit. Veuillez vérifier le nom ou réessayer plus tard.")
-
-    else:
-        st.write('Veuillez cliquer sur le bouton pour valider !') 
 
 st.divider() 
 #########################
